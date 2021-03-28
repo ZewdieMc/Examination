7f03ec004657e8c3d78d5f73ae4d11a83f2133b1
ZewdieMc <zewdiehabtie26@gmail.com>
2021-03-28 09:47:34 +0300

    login modules addeded

---
+++ box/footer.php
@@ -0,0 +1,12 @@
+
+
+<!--Footer Starts Here-->
+        <footer class="fixed-bottom"  >
+            
+                <center>
+                    &copy; <?php echo date('Y'); ?>. All Rights Reserved.    
+                    Powered by <a href="http://www.dtu.edu.et" title="Debre Tabor University" target="_blank">Debrer Tabor University</a>.
+                </center>
+            </footer>
+        <!--Footer Ends Here-->
+</html>
